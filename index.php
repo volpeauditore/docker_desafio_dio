026dc7b2e74f5b792cc23260ed9f1394de4d7f3a
volpeauditore <168099162+volpeauditore@users.noreply.github.com>
2025-03-28 08:19:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "localhost";
 $username = "root";
 $password = "Senha123";
 $database = "meubanco";
